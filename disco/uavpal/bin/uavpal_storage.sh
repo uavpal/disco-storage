7ccaa280a293c18d048d2013a8982716a14292ad
SoftIce <softice@uavpal.com>
2019-04-30 22:49:35 +0800

    added color feedback

--- disco/uavpal/bin/uavpal_storage.sh
+++ disco/uavpal/bin/uavpal_storage.sh
@@ -14,11 +14,17 @@ if [ "$1" == "add" ]; then
 	mount -t vfat -o rw,noatime /dev/${partition} ${media_path}
 	if [ $? -ne 0 ]; then
 		ulogger -s -t uavpal_storage "... could not mount USB mass storage partition ${partition} - please ensure the file system is FAT32 (and not exFAT!). Exiting!"
+		ldc set_pattern demo_low_bat true
+		sleep 3
+		ldc set_pattern idle true
 		exit 1
 	fi
 	ulogger -s -t uavpal_storage "... partition ${partition} has been mounted successfully"
 	diskfree=$(df -h | grep ${partition})
 	ulogger -s -t uavpal_storage "... photos and videos will now be stored on the USB mass storage device (capacity: $(echo $diskfree | awk '{print $2}') / available: $(echo $diskfree | awk '{print $4}'))"
+	ldc set_pattern color_wheel true
+	sleep 5
+	ldc set_pattern idle true
 elif [ "$1" == "remove" ]; then
 	ulogger -s -t uavpal_storage "... disk ${disk} has been removed"
 	umount -f ${media_path}
@@ -27,4 +33,7 @@ elif [ "$1" == "remove" ]; then
 	mkdir ${media_path}
 	chmod 755 ${media_path}
 	chown root:root ${media_path}
+	ldc set_pattern demo_low_bat true
+	sleep 3
+	ldc set_pattern idle true
 fi
