23c1dd910e4907cdd812e5173e9e3672e18b2200
SoftIce <softice@uavpal.com>
2019-05-01 16:09:42 +0800

    made LED functions re-usable

--- disco/uavpal/bin/uavpal_storage.sh
+++ disco/uavpal/bin/uavpal_storage.sh
@@ -5,6 +5,18 @@ partition="$(echo $2 | cut -d "/" -f 3)"
 disk="$(echo $partition | rev | cut -c 2- | rev)"
 media_path="/data/ftp/internal_000/Disco/media"
 
+# functions
+led_indicate_external () {
+	ldc set_pattern color_wheel true
+	sleep 5
+	ldc set_pattern idle true
+}
+
+led_indicate_internal () {
+	ldc set_pattern demo_low_bat true
+	sleep 3
+	ldc set_pattern idle true
+}
 if [ "$1" == "add" ]; then
 	last_partition=$(ls /dev/${disk}? | tail -n 1)
 	if [ "$last_partition" != "/dev/${partition}" ]; then
@@ -14,17 +26,13 @@ if [ "$1" == "add" ]; then
 	mount -t vfat -o rw,noatime /dev/${partition} ${media_path}
 	if [ $? -ne 0 ]; then
 		ulogger -s -t uavpal_storage "... could not mount USB mass storage partition ${partition} - please ensure the file system is FAT32 (and not exFAT!). Exiting!"
-		ldc set_pattern demo_low_bat true
-		sleep 3
-		ldc set_pattern idle true
+		led_indicate_internal
 		exit 1
 	fi
 	ulogger -s -t uavpal_storage "... partition ${partition} has been mounted successfully"
 	diskfree=$(df -h | grep ${partition})
 	ulogger -s -t uavpal_storage "... photos and videos will now be stored on the USB mass storage device (capacity: $(echo $diskfree | awk '{print $2}') / available: $(echo $diskfree | awk '{print $4}'))"
-	ldc set_pattern color_wheel true
-	sleep 5
-	ldc set_pattern idle true
+	led_indicate_external
 elif [ "$1" == "remove" ]; then
 	ulogger -s -t uavpal_storage "... disk ${disk} has been removed"
 	umount -f ${media_path}
@@ -33,7 +41,5 @@ elif [ "$1" == "remove" ]; then
 	mkdir ${media_path}
 	chmod 755 ${media_path}
 	chown root:root ${media_path}
-	ldc set_pattern demo_low_bat true
-	sleep 3
-	ldc set_pattern idle true
+	led_indicate_internal
 fi
